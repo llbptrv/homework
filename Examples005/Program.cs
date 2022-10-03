c7354e17000d2ce7904e6643cc84a1635434b666
liubov <llbptrv@yandex.ru>
2022-10-03 15:57:24 +0300

    файл c if else

---
+++ Examples005/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя:");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+{
+   Console.Write("Ура, наконец-то это Маша!"); 
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
