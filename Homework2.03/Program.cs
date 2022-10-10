78b0e4aea58b846a260df9cab23dc6b04a0084c5
liubov <llbptrv@yandex.ru>
2022-10-10 21:44:18 +0300

    дз к семинару 2, номер 3

---
+++ Homework2.03/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("введите число 1-7, соответствующее дню недели:");
+int n= Convert.ToInt32(Console.ReadLine());
+if (n>5 && n<8)
+    Console.WriteLine("ура, выходной");
+else if (n<5 && n>0)
+    Console.WriteLine("это не выходной");
+else if (n>7 || n<1)
+    Console.WriteLine("число не соответствует дню недели");
