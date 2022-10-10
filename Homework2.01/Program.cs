b675be735f6989d66104094d015abadb26c0d0f4
liubov <llbptrv@yandex.ru>
2022-10-10 21:40:55 +0300

    дз к семинару 2, номер 1

---
+++ Homework2.01/Program.cs
@@ -0,0 +1,7 @@
+﻿//задача 10
+Console.Write("Введите трехзначное число: ");
+int n= Convert.ToInt32(Console.ReadLine());
+if (n/100!=0 && n<1000)
+    Console.WriteLine($"Вы ввели:{n}\nВторая цифра:{(n/10)%10}");
+else if(n/100==0 || n/100>9)
+    Console.WriteLine("Вы ввели не трехзначное число, научитесь читать!");
