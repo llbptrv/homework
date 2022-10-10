efc272269cebb88a33be58792b3efb334c3b3207
liubov <llbptrv@yandex.ru>
2022-10-10 21:43:56 +0300

    дз к семинару 2, номер 2

---
+++ Homework2.02/Program.cs
@@ -0,0 +1,19 @@
+﻿//задача 10
+Console.Write("Введите число: ");
+int n= Convert.ToInt32(Console.ReadLine());
+
+if (n>99 && n<1000)
+{
+    Console.WriteLine($"Вы ввели:{n}\nТретья цифра:{n%10}");
+}
+else if(n<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else if(n>999)
+{
+    while (n>1000)
+        n=n/10;
+    Console.WriteLine($"Вы ввели:{n}\nТретья цифра:{n%10}");
+}
+   
