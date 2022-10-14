bdb2cd95f937c7e1cd5043cab7f2ca26e0c6e9f4
liubov <llbptrv@yandex.ru>
2022-10-15 00:29:30 +0300

    дз к семинару 3, номер 1

---
+++ Homework3.03/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("введите пятизначное число: ");
+int n=Convert.ToInt32(Console.ReadLine());
+int a1=0;
+int a2=0;
+
+a1=(n/1000)%10;
+a2=(n%100)/10;
+
+if(n/10000==n%10 && a1==a2)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
