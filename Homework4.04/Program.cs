07f8c0411f6b360ebd82340709db6e46c525b45e
liubov <llbptrv@yandex.ru>
2022-10-19 20:15:37 +0300

    дз 4, номер 4

---
+++ Homework4.04/Program.cs
@@ -0,0 +1,38 @@
+﻿
+Console.WriteLine("введите массив чисел(5), каждое из которых не более 100, указав последним числом ноль");
+int[] array = new int[5];
+int length=array.Length;
+int index=0;
+
+int max=0;
+int max1=0;
+
+while(index<length)
+{
+    array[index]= Convert.ToInt32(Console.ReadLine());
+    index++;
+}
+Console.WriteLine("["+string.Join("," , array)+"]");
+
+while(index<length)
+{
+    if(array[index]>max)  
+    {
+        max=array[index]; 
+        index++;
+    }
+}
+Console.WriteLine($"наибольшее число:{max} ");
+    
+array[max]=0;
+
+while(index<length)
+{
+    if(array[index]>max1) 
+    {
+        max1=array[index]; 
+        index++;
+    }
+}
+
+Console.WriteLine($"второе наибольшее число:{max1} ");
