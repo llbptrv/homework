4b2c8cd374a8f67d32b9fc6169fd61585e8fb2a3
liubov <llbptrv@yandex.ru>
2022-10-19 18:46:23 +0300

    дз 4, номер 2

---
+++ Homework4.02/Program.cs
@@ -0,0 +1,12 @@
+﻿int n= Convert.ToInt32(Console.ReadLine());
+int m= Convert.ToInt32(Console.ReadLine());
+int i=0;
+int result=1;
+
+while(i<m)
+{
+    result=result*n;
+    i++;
+}
+
+Console.WriteLine(result);
