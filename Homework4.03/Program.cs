d758e3c324a7f678644ccfa83030276c575d1740
liubov <llbptrv@yandex.ru>
2022-10-19 18:56:15 +0300

    дз 4, номер 3

---
+++ Homework4.03/Program.cs
@@ -0,0 +1,11 @@
+﻿int[] array = new int[8];
+int length=array.Length;
+int index=0;
+
+while(index<length)
+{
+    array[index]= Convert.ToInt32(Console.ReadLine());
+    index++;
+}
+
+Console.WriteLine("["+string.Join("," , array)+"]");
