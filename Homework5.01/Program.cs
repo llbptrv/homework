8b50e22a9c586284fb1b5ecad531ab829150056c
liubov <llbptrv@yandex.ru>
2022-10-28 13:37:20 +0300

    дз 5, задача 34

---
+++ Homework5.01/Program.cs
@@ -0,0 +1,28 @@
+﻿//задача 34
+void InputArray(int[] array)
+{
+    for(int i=0; i<array.Length; i++)
+    {
+        array[i]=new Random().Next(100,1000);
+    }
+}
+int CheckArray(int[] array)
+{
+    int result=0;
+    for(int i=0; i<array.Length; i++)
+    {
+        if(array[i]%2==0)
+        {
+            result++;
+        }
+    
+    }
+    return result;
+}
+
+
+int[] array = new int[4];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", " , array)}]");
+CheckArray(array);
+Console.WriteLine($"количество элементов: {CheckArray(array)}");
