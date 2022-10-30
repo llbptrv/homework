9445d0acba1e3aa506c3bc94ffabaf5f719f2bfd
liubov <llbptrv@yandex.ru>
2022-10-30 14:30:34 +0300

    дз 6, задание 41

---
+++ Homework6.01/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.Write("сколько чисел вы хотите ввести: ");
+int n=Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите числа: ");
+
+int result=0;
+int i=0;
+while(i<n)
+{
+    int m= Convert.ToInt32(Console.ReadLine());
+    if(m>0)
+    {
+        result++;
+    }
+    i++;
+}
+Console.WriteLine($"пользователь ввел {result} чис(ел)(ла)(ло) больше 0");
