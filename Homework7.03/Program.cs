ba88ecc340015ccbe83cdfacf6a5f87c49ccc35c
liubov <llbptrv@yandex.ru>
2022-11-04 14:02:54 +0300

    дз7 номер 52

---
+++ Homework7.03/Program.cs
@@ -0,0 +1,34 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i+j;
+                       
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+    
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, m];
+InputMatrix(matrix);
+
+for (int j = 0; j < matrix.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        sum += matrix[i, j];
+    }
+    Console.WriteLine($"Cреднее арифметическое столбца {j}: { sum / matrix.GetLength(0)} ");
+}
+
