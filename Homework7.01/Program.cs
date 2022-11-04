53cc8a75aae86e9c480cd7a35f85f4e71dcf06ee
liubov <llbptrv@yandex.ru>
2022-11-04 11:00:27 +0300

    дз 7, задание 47

---
+++ Homework7.01/Program.cs
@@ -0,0 +1,22 @@
+﻿void InputMatrix(double[,] matrix)
+{
+    Random random = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round(random.NextDouble()*100, 2);
+                       
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+    
+}
+
+Console.Write("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите кол-во столбцов: ");
+int m = Convert.ToInt32(Console.ReadLine());
+double[,] matrix = new double[n, m];
+InputMatrix(matrix);
