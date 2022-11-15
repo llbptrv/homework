9518f4ccfe8b5d55383466152303517a0c455e55
liubov <llbptrv@yandex.ru>
2022-11-15 17:27:51 +0300

    дз 8, задача 61

---
+++ Homework8.06/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.Write("Введите максимальное число элементов в треугольнике Паскаля: ");
+int a=Convert.ToInt32(Console.ReadLine());
+int n=0;
+int b=0;
+
+int factorial(int a)
+{
+    int n=1;
+    int x=1;
+    for (n=1; n<=a; n++)
+    {
+        x=x*n;
+    }
+    return x;
+}
+for (n=0; n<a; n++)
+{
+    for(b=0; b<=(a-n); b++)
+    {
+        Console.Write(" ");
+    }
+
+    for(b=0; b<=n; b++)
+    {
+        Console.Write(" ");
+        Console.Write(factorial(n)/(factorial(b)*factorial(n-b)));
+        
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+}
+Console.ReadLine();
