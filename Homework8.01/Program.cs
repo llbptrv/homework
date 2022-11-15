a5287a6597db9d279bb19b8e44f2484993435a39
liubov <llbptrv@yandex.ru>
2022-11-15 17:52:14 +0300

    дз 8, задача 54

---
+++ Homework8.01/Program.cs
@@ -0,0 +1,42 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 5);
+                       
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+    
+}
+
+
+
+void RealiseMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =0; j < matrix.GetLength(1); j++)
+        {   
+            for (int k=0;  k<matrix.GetLength(1)-1; k++)
+            {
+                if(matrix[i,k]< matrix[i, k+1])
+                {
+                    int temp = matrix[i, k+1];
+                    matrix [i, k+1] = matrix[i,k];
+                    matrix[i,k]= temp;
+                }
+            }
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+}
+int[,] matrix = new int[3,3];
+InputMatrix(matrix);
+Console.WriteLine();
+RealiseMatrix(matrix);
