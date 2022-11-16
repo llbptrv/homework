5819cf845441ab628aab98b082f358b13c1cc0f1
liubov <llbptrv@yandex.ru>
2022-11-16 12:12:00 +0300

    дз 8, задача 56

---
+++ Homework8.02/Program.cs
@@ -0,0 +1,45 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 5);
+                       
+            Console.Write($"{matrix[i, j]} \t");
+        }
+    Console.WriteLine();
+    }
+    
+}
+
+void RealiseMatrix(int[,] matrix)
+{
+    int[] SumRow = new int [matrix.GetLength(0)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j =0; j < matrix.GetLength(1); j++)
+        {   
+            SumRow[i]+= matrix[i,j];
+
+        }
+    }
+    int t = SumRow[0];
+    int k=0;
+    for(int i =0;  i<SumRow.Length; i++ )
+    {
+        if(SumRow[i]<t)
+        {
+            t=SumRow[i];
+            k=i+1;
+        }
+    }
+    Console.WriteLine("минимальная сумма элементов находится в строке "+ k);
+    Console.WriteLine("минимальная сумма элементов = "+ t);
+}
+
+int[,] matrix = new int[3,3];
+InputMatrix(matrix);
+Console.WriteLine();
+RealiseMatrix(matrix);
