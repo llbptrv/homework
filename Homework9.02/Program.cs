04394d9aaece588481d54dbac8ed9cea80785999
liubov <llbptrv@yandex.ru>
2022-11-17 11:35:48 +0300

    дз 9, задача 68

---
+++ Homework9.02/Program.cs
@@ -0,0 +1,21 @@
+﻿//Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+Console.Write("Введите число больше или = 0: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число больше или = 0: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (m<0 || n<0)
+    Console.Write("Научитесь читать и введите число больше или = 0: ");
+else
+{
+    int ResultNumbers(int m, int n)
+    {
+        if (m==0)
+            return n+1;
+        if(n==0)
+            return ResultNumbers(m-1, 1);
+        return ResultNumbers(m-1, ResultNumbers(m, n-1));
+        
+    }
+    Console.WriteLine(ResultNumbers(m, n));
+}    
