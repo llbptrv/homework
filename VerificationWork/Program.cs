57f2b80febc3ac9c18e8022c7abff127dfedd300
liubov <llbptrv@yandex.ru>
2022-11-28 14:25:29 +0300

    код

---
+++ VerificationWork/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] days = { "Sun", "22", "Monday", "Tuersday", "Wednesday", "Thirsday", "Fri", "Saturday" };
+Console.WriteLine("изначальный массив: ");
+for (int i = 0; i < days.Length; i++)
+{
+    
+    Console.Write($"{days[i]},  ");
+}
+Console.WriteLine();
+Console.WriteLine("итоговый массив: ");
+for (int i = 0; i < days.Length; i++)
+{
+
+    if (days[i].Length<4)
+    {
+        Console.Write($"{days[i]}  ");
+    }
+}
